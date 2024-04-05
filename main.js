91545b2914bc783d0554f36ff2535cf21d57b584
Daiane <117860490+daianealura@users.noreply.github.com>
2024-04-05 08:15:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -66,3 +66,60 @@ function calculaTempo(tempoObjetivo) {
   return segundos;
 }
 
+let dias = Math.floor(horas / 24);
+for (let i=0; i<contadores.length;i++){
+    // contadores[i].textContent = calculaTempo(tempos[i]);   
+    }
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+        return [0,0,0,0];
+    }
+    function atualizaCronometro(){
+        document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+        document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+        document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+        document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+    
+        for (let i=0; i<contadores.length;i++){
+           // contadores[i].textContent = calculaTempo(tempos[i]);   
+        }
+        if (tempoFinal > 0){
+            return [dias,horas,minutos,segundos];
+        } else {
+            return [0,0,0,0];
+        }
+          
+        function atualizaCronometro(){ 
+            document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0];
+            document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];  
+            document.getElementById("min0").textContent = calculaTempo(tempos[1])[2];
+            document.getElementById("seg0").textContent = calculaTempo(tempos[1])[3];
+            for (let i=0; i<contadores.length;i++){
+                //contadores[i].textContent = calculaTempo(tempos[i]);   
+            }
+        }
+        function calculaTempo(tempoObjetivo) {
+            let tempoAtual = new Date();
+            let tempoFinal = tempoObjetivo - tempoAtual;
+            let segundos = Math.floor(tempoFinal / 1000);
+            let minutos = Math.floor(segundos / 60);
+            let horas = Math.floor(minutos / 60);
+            let dias = Math.floor(horas / 24);
+        
+            segundos %= 60;
+            minutos %= 60;
+            horas %= 24;
+            if (tempoFinal > 0){
+                return [dias,horas,minutos,segundos]
+            } else {
+                return [0,0,0,0];
+            }
+        }
+        document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+
+                          
